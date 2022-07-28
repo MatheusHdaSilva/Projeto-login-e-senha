510c3c4b6e5bb7fad8850eb668b061694725162a
Matheus Henrique Da Silva <matheush5529@gmail.com>
2022-07-27 23:47:45 -0300

    definindo tarefas

--- Protótipo.py
+++ Protótipo.py
@@ -5,8 +5,8 @@ print('''Você já tem Login ou Deseja realizar o cadastro?
 [ 1 ] Já possui cadastro
 [ 2 ] Não tenho cadastro''')
 select = int(input('Selecione uma das opções acima: '))
-if select == 1:
-    print('ok')
+if select == 1: 
+    
 
 
 
@@ -20,3 +20,4 @@ if select == 1:
 #Descobrir se é cadastro ou login 
 #ferificar se os campos foram preenchidos 
 #verificar se o email é valido 
+#usar strip / Achar comando que identifique a quantidade de caracteres em uma str 
