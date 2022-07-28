9c4fc32c7278147491a68a59e165575fea0d0381
Matheus Henrique Da Silva <matheush5529@gmail.com>
2022-07-27 23:17:55 -0300

    Cadastro ou login

---
+++ Protótipo.py
@@ -0,0 +1,22 @@
+from select import select
+
+
+print('''Você já tem Login ou Deseja realizar o cadastro?
+[ 1 ] Já possui cadastro
+[ 2 ] Não tenho cadastro''')
+select = int(input('Selecione uma das opções acima: '))
+if select == 1:
+    print('ok')
+
+
+
+
+
+
+
+
+
+
+#Descobrir se é cadastro ou login 
+#ferificar se os campos foram preenchidos 
+#verificar se o email é valido 
