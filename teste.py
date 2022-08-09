29015d5589ee9e0611b75ade83dd5944adee74d2
Matheus Henrique Da Silva <matheush5529@gmail.com>
2022-08-09 20:59:20 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,2 @@
 from collections import Counter
-from email.contentmanager import ContentManager
-a = str(input('Preecha o campo: '))
-conta = Counter(a)
-print(conta)
+a = str(input('Preecha o campo: '.count('a')))
